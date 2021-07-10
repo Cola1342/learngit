c628751ffb4e92a84a5cbbbc5d9dc8af8d7dc662
Joaquin <387211948@qq.com>
2021-07-10 16:39:01 +0800

    add list operator

---
+++ listoperator.py
@@ -0,0 +1,15 @@
+my_list = [3, 1, 2, 5, 4, 9, 7, 2]
+
+print(my_list[2])
+
+my_list[3] = 10
+print(my_list)
+
+my_list.append(6)
+print(my_list)
+
+my_list.insert(5, 11)
+print(my_list)
+
+my_list.remove(2)
+print(my_list)
